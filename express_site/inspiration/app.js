b1331f9ff548e6e5d4c1305c840096eba2b712b7
Andy Yu <andyyu199623@yahoo.com>
2016-06-14 15:04:28 -0400

    background color to views

--- express_site/inspiration/app.js
+++ express_site/inspiration/app.js
@@ -3,19 +3,22 @@ var exphbs  = require('express-handlebars');
 
 var app = express();
 
+var quotes = ["You are inspired when you find inspiration", "If you seek inspiration, look for it", "I am always inspired by inspirational quotes"];
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static( __dirname + '/public'));
 
-app.get('/', function(req, res) {     
+app.get('/', function(req, res) { 
 	res.render('home'); 
 }); 
 app.get('/about', function(req,res){     
 	res.render('about'); 
 }); 
 app.get('/inspiration', function(req,res){     
-	res.render('inspiration' ); 
+	var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+	res.render('inspiration', { inspirational: randomQuote });
 });
 app.listen(3000, function () {   
 	console.log('Inspiration app listening on port 3000!'); 
--- express_site/inspiration/public/javascript/functions.js
+++ express_site/inspiration/public/javascript/functions.js
@@ -4,6 +4,10 @@ function init() {
 	var page = document.URL.split('/').join(' ').trim().split(" ");
 	//get the current page which mostly is in the ending of the url
 	page = page[page.length-1];
+
+	if(window.location.host == page) {
+		page = "main";
+	}
 	//get the container element of the page
 	var container = document.getElementsByClassName("container")[0];
 	//set the container id revelant to the page it's on
