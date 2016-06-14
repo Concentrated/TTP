76601f7797e51c2da395c3e312583161c3e1a680
Andy Yu <andyyu199623@yahoo.com>
2016-06-14 14:31:59 -0400

    delete modules

--- express_site/inspiration/public/javascript/functions.js
+++ express_site/inspiration/public/javascript/functions.js
@@ -4,12 +4,15 @@ function init() {
 	var page = document.URL.split('/').join(' ').trim().split(" ");
 	//get the current page which mostly is in the ending of the url
 	page = page[page.length-1];
-	var container;
-	if(window.location.hostname == page) {
-		 page = "main"
-	} else {
-		//get the container element of the
-		 page = document.getElementsByClassName("container")[0];
+
+	var container = document.getElementsByClassName("container")[0];
+
+	console.log(page);
+	console.log(window.location.hostname); 
+
+	var port = ":3000";
+	if((window.location.hostname+port) == page) {
+		page  = "main"
 	}
 	//set the container id revelant to the page it's on
 	container.id = page;
